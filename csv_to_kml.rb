6dd3b84ff493c402a25da40fdd5e1ce6d35dda38
AndreaCracchiolo <andreac@freezanz.it>
2024-01-26 23:44:52 +0100

    small refactoring. Added logger and error handling

--- csv_to_kml.rb
+++ csv_to_kml.rb
@@ -1,77 +1,95 @@
+#!/usr/bin/env ruby
 require "csv"
 require "rexml/document"
 require "net/http"
 require "json"
 require "cgi"
-require "debug"
+require "logger"
+
+# Initialize constants
+CSV_PATH = ARGV[0] || "impianti.csv"
+KML_PATH = ARGV[1] || "lista_impianti.kml"
+NAME_FIELD = ARGV[2] || "Nome Cognome Cliente CAP"
+ADDRESS_FIELD = ARGV[3] || "Indirizzo Cap Comune"
+API_KEY = ENV["MAPS_API"]
+
+# Set up logger
+logger = Logger.new(STDOUT)
 
 # Function to geocode address
-def geocode(address)
-  api_key = ENV["MAPS_API"]
+def geocode(address, api_key)
   base_uri = "https://maps.googleapis.com/maps/api/geocode/json"
   uri = URI("#{base_uri}?address=#{CGI.escape(address)}&key=#{api_key}")
   response = Net::HTTP.get(uri)
   json = JSON.parse(response)
-  p "Response data: #{json["status"]}"
+  logger.info "Response data: #{json["status"]}"
   if json["status"] == "OK"
     json["results"][0]["geometry"]["location"]
   else
     nil
   end
+rescue StandardError => error
+  logger.fatal "Failed to geocode address: #{error.message}"
+  nil
 end
 
-# Read the CSV file
-csv_path = ARGV[0]
-kml_path = ARGV[1]
-#if path doesnt exist, return error
-unless File.exist?(csv_path)
-  puts "File #{csv_path} does not exist."
+# Load CSV file
+begin
+  logger.info "Loading csv file: #{CSV_PATH}"
+  data = CSV.read(CSV_PATH, headers: true, encoding: "bom|utf-8")
+  logger.info "Loaded csv file: #{CSV_PATH}"
+rescue Errno::ENOENT => error
+  logger.fatal "Failed to load csv file: #{error.message}"
   exit 1
 end
 
-p "Loading csv file: #{csv_path}"
-data = CSV.read(csv_path, headers: true, encoding: "bom|utf-8")
-p "Loaded csv file: #{csv_path}"
-
 # Create a new XML document
-kml = REXML::Document.new
-kml << REXML::XMLDecl.new
-
-$name_field = ARGV[2] || "Nome Cognome Cliente CAP"
-$address_field = ARGV[3] || "Indirizzo Cap Comune"
+kml_doc = REXML::Document.new
+kml_doc << REXML::XMLDecl.new
 
 # Create a root element
-root = kml.add_element "kml", { "xmlns" => "http://www.opengis.net/kml/2.2" }
+root = kml_doc.add_element "kml", { "xmlns" => "http://www.opengis.net/kml/2.2" }
 
 # Create a Document element
 document = root.add_element "Document"
 
 # Iterate over each row in the CSV file
 data.each do |row|
-  break if row["#{$name_field}"].nil?
-  # Geocode the address
-  location = geocode(row["#{$address_field}"])
-  if location
-    # Create a Placemark for each row
-    placemark = document.add_element "Placemark"
-
-    # Add the name
-    placemark.add_element("name").add_text row["#{$name_field}"]
-
-    # Add the description with all the other data
-    description = placemark.add_element "description"
-    row.headers.each do |header|
-      next if header == $name_field || header == $address_field
-      description.add_text "#{header}: #{row[header.to_s]}\n"
-    end
-
-    # Add the Point with the coordinates
-    point = placemark.add_element "Point"
-    point.add_element("coordinates").add_text "#{location["lng"]},#{location["lat"]}"
+  logger.info "Processing row: #{row}"
+  #AVOID EOF
+  if row[NAME_FIELD].nil? || row[ADDRESS_FIELD].nil?
+    logger.info "No other name or address fields found! Parsing done"
+    break
   end
+
+  # Geocode the address
+  location = geocode(row[ADDRESS_FIELD], API_KEY)
+  next unless location
+
+  # Create a Placemark for each row
+  placemark = document.add_element "Placemark"
+
+  # Add the name
+  placemark.add_element("name").add_text row[NAME_FIELD]
+
+  # Add the description with all the other data
+  description = placemark.add_element "description"
+  row.headers.each do |header|
+    next if [NAME_FIELD, ADDRESS_FIELD].include?(header)
+    description.add_text "#{header}: #{row[header.to_s]}\n"
+  end
+
+  # Add the Point with the coordinates
+  point = placemark.add_element "Point"
+  point.add_element("coordinates").add_text "#{location["lng"]},#{location["lat"]}"
 end
 
-# Write the KML to a file
-File.open(kml_path, "w") { |f| f.write(kml.to_s) }
-
-p "Done! Thanks for using csv_to_kml!"
+# Write the KML file
+begin
+  logger.info "Writing KML file: #{KML_PATH}"
+  File.open(KML_PATH, "w") { |f| f.write(kml_doc.to_s) }
+  logger.info "Done! Thanks for using csv_to_kml!"
+rescue StandardError => error
+  logger.error "Failed to write KML file: #{error.message}"
+  exit 1
+end
